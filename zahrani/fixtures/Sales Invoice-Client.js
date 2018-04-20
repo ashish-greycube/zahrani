c737fcb0c165d90132fec44f0356ad0d94d18f82
ashish-greycube <mr.ashish.shah@gmail.com>
2018-04-20 09:04:55 +0530

    check put in sales invoice script

--- zahrani/fixtures/Sales Invoice-Client.js
+++ zahrani/fixtures/Sales Invoice-Client.js
@@ -1,19 +1,23 @@
-frappe.ui.form.on("Sales Invoice", "refresh", function (frm) {
-    frappe.call({
-        method: "erpnext.stock.get_item_details.get_pos_profile",
-        args: {
-            company: frappe.defaults.get_user_default("Company"),
-            user: frappe.session.user_email
-        },
-        callback: function (r) {
-            if (r.message) {     
-                if (cur_frm.doc.branch == undefined && r.message.branch != '') {
-                    cur_frm.set_value("branch",r.message.branch);
-                }
-                if (cur_frm.doc.invoice_type =="" && r.message.invoice_type != '') {
-                    cur_frm.set_value("invoice_type", r.message.invoice_type);
+frappe.ui.form.on("Sales Invoice", "is_pos", function (frm) {
+    if (cur_frm.doc.is_pos == 1 && cur_frm.doc.docstatus == 0) {
+
+        frappe.call({
+            method: "erpnext.stock.get_item_details.get_pos_profile",
+            args: {
+                company: frappe.defaults.get_user_default("Company"),
+                user: frappe.session.user_email
+            },
+            callback: function (r) {
+                if (r.message) {
+                    if (cur_frm.doc.branch == undefined && r.message.branch != '') {
+                        cur_frm.set_value("branch", r.message.branch);
+                    }
+                    if (cur_frm.doc.invoice_type == "" && r.message.invoice_type != '') {
+                        cur_frm.set_value("invoice_type", r.message.invoice_type);
+                    }
                 }
             }
-        }
-    });
+        });
+
+    }
 });
